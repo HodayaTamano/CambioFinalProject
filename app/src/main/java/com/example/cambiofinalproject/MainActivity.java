a288a9d89258c451262dbcf4448b4dbd48e05628
Hodaya Tamano <hodayat377@gmail.com>
2019-07-30 15:52:50 +0300

    Update activity_main.xml

--- app/src/main/java/com/example/cambiofinalproject/MainActivity.java
+++ app/src/main/java/com/example/cambiofinalproject/MainActivity.java
@@ -23,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     ImageButton playerCard3;
     ImageButton playerCard4;
 
+    int counter = 0;
+    boolean cardIsPressed = true;
+    boolean secondCardIsPressed = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         computerCard2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 //          clicking on the back of the card and card displays
+
                 int imageId = getResources().getIdentifier(Computer.computerCards[1].toString(),"drawable", getPackageName());
                 Drawable myDrawable = getResources().getDrawable(imageId);
                 computerCard2.setImageDrawable(myDrawable);
@@ -74,49 +79,70 @@ public class MainActivity extends AppCompatActivity {
         });
 
 ///////////////////////////////////////////////// Player Cards ////////////////////////////////////////////////////////////
-        playerCard1 = (ImageButton) findViewById(R.id.playerCard1);
-        playerCard1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-//          clicking on the back of the card and card displays
-                int imageId = getResources().getIdentifier(Game.playerCards[0].toString(),"drawable", getPackageName());
-                Drawable myDrawable = getResources().getDrawable(imageId);
-                playerCard1.setImageDrawable(myDrawable);
-                Toast.makeText(MainActivity.this,Game.playerCards[0].toString(),Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        playerCard2 = (ImageButton) findViewById(R.id.playerCard2);
-        playerCard2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            playerCard1 = (ImageButton) findViewById(R.id.playerCard1);
+            playerCard1.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 //          clicking on the back of the card and card displays
-                int imageId = getResources().getIdentifier(Game.playerCards[1].toString(),"drawable", getPackageName());
-                Drawable myDrawable = getResources().getDrawable(imageId);
-                playerCard2.setImageDrawable(myDrawable);
-                Toast.makeText(MainActivity.this,Game.playerCards[1].toString(),Toast.LENGTH_SHORT).show();
-            }
-        });
+                    if (cardIsPressed) {
+                        int imageId = getResources().getIdentifier(Game.playerCards[0].toString(), "drawable", getPackageName());
+                        Drawable myDrawable = getResources().getDrawable(imageId);
+                        playerCard1.setImageDrawable(myDrawable);
+                        Toast.makeText(MainActivity.this, Game.playerCards[0].toString(), Toast.LENGTH_SHORT).show();
+                        cardIsPressed = false;
+                    }
+                    else{
+                        int imageId = getResources().getIdentifier("back", "drawable", getPackageName());
+                        Drawable myDrawable = getResources().getDrawable(imageId);
+                        playerCard1.setImageDrawable(myDrawable);
+                        cardIsPressed = true;
+                    }
+                }
+            });
 
-        playerCard3 = (ImageButton) findViewById(R.id.playerCard3);
-        playerCard3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            playerCard2 = (ImageButton) findViewById(R.id.playerCard2);
+            playerCard2.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 //          clicking on the back of the card and card displays
-                int imageId = getResources().getIdentifier(Game.playerCards[2].toString(),"drawable", getPackageName());
-                Drawable myDrawable = getResources().getDrawable(imageId);
-                playerCard3.setImageDrawable(myDrawable);
-                Toast.makeText(MainActivity.this,Game.playerCards[2].toString(),Toast.LENGTH_SHORT).show();
-            }
-        });
+                    if (cardIsPressed){
+                        int imageId = getResources().getIdentifier(Game.playerCards[1].toString(), "drawable", getPackageName());
+                        Drawable myDrawable = getResources().getDrawable(imageId);
+                        playerCard2.setImageDrawable(myDrawable);
+                        Toast.makeText(MainActivity.this, Game.playerCards[1].toString(), Toast.LENGTH_SHORT).show();
+                        cardIsPressed = false;
+                    }else{
+                        int imageId = getResources().getIdentifier("back", "drawable", getPackageName());
+                        Drawable myDrawable = getResources().getDrawable(imageId);
+                        playerCard1.setImageDrawable(myDrawable);
+                        cardIsPressed = true;
+                    }
 
-        playerCard4 = (ImageButton) findViewById(R.id.playerCard4);
-        playerCard4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+                }
+            });
+
+            playerCard3 = (ImageButton) findViewById(R.id.playerCard3);
+            playerCard3.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 //          clicking on the back of the card and card displays
-                int imageId = getResources().getIdentifier(Game.playerCards[3].toString(),"drawable", getPackageName());
-                Drawable myDrawable = getResources().getDrawable(imageId);
-                playerCard4.setImageDrawable(myDrawable);
-                Toast.makeText(MainActivity.this,Game.playerCards[3].toString(),Toast.LENGTH_SHORT).show();
-            }
-        });
+                    int imageId = getResources().getIdentifier(Game.playerCards[2].toString(),"drawable", getPackageName());
+                    Drawable myDrawable = getResources().getDrawable(imageId);
+                    playerCard3.setImageDrawable(myDrawable);
+                    Toast.makeText(MainActivity.this,Game.playerCards[2].toString(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            playerCard4 = (ImageButton) findViewById(R.id.playerCard4);
+            playerCard4.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+//          clicking on the back of the card and card displays
+                    int imageId = getResources().getIdentifier(Game.playerCards[3].toString(),"drawable", getPackageName());
+                    Drawable myDrawable = getResources().getDrawable(imageId);
+                    playerCard4.setImageDrawable(myDrawable);
+                    Toast.makeText(MainActivity.this,Game.playerCards[3].toString(),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
 
 //        backComputerCard4 = (ImageButton) findViewById(R.id.computerCard4);
 //        backComputerCard4.setOnClickListener(new View.OnClickListener() {
